a7d791ecf07d7e605af3eb5b61be9681922581a5
ridhaomar <labbar2010@gmail.com>
2020-06-26 16:00:21 +0200

    Initial Commit

---
+++ file_handler.js
@@ -0,0 +1,25 @@
+var openFile = function(event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function(){
+      var text = reader.result;
+    //   var node = document.getElementById('output');
+    //   node.innerText = text;
+      
+      const allLines = text.split('\n');
+      allLines.forEach((line) => {
+        // console.log(line);
+        //   console.log('From (' + line.substring(0, 1) + ') to (' + line.substring(2, 3) + ') = ' + line.substring(4, 5));
+
+        //   graph.push({from: line.substring(0, 1), to: line.substring(2, 3), equal: line.substring(4, 5)});
+        graph.push([line.substring(0, 1).toUpperCase(), line.substring(2, 3).toUpperCase(), parseInt(line.substring(4, 5))])
+    });
+    
+    // console.log(x[0].from + "  " + x[0].to  + "  " + x[0].equal );
+    // console.log(graph);
+
+    };
+    reader.readAsText(input.files[0]);
+    alert(dddd)
+};
